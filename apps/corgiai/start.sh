406a8d2fee7293a58c779bab0d192c5da3d70441
BLaZeKiLL <laldevashish@gmail.com>
2023-11-20 08:58:04 +0000

    startup changes

--- apps/corgiai/start.sh
+++ apps/corgiai/start.sh
@@ -1,3 +1,5 @@
+echo CORGI APP STARTUP
+
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 /app/tailscale login --hostname=azure-corgiai
 /app/tailscale up 
@@ -6,4 +8,4 @@ API_PY_CHAT=http://$(/app/tailscale ip -4 codeblazeserver)
 
 echo $API_PY_CHAT
 
-node -r dotenv/config build
+ALL_PROXY=socks5://localhost:1055/ node -r dotenv/config build
