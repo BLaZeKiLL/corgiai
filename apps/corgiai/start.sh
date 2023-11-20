81adb4013d7a8dccbd8631e8bb1f154203a9b1aa
BLaZeKiLL <laldevashish@gmail.com>
2023-11-20 08:25:03 +0000

    remove qr

--- apps/corgiai/start.sh
+++ apps/corgiai/start.sh
@@ -1,5 +1,5 @@
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-/app/tailscale login --qr --hostname=azure-corgiai
+/app/tailscale login --hostname=azure-corgiai
 /app/tailscale up 
 
 API_PY_CHAT=http://$(/app/tailscale ip -4 codeblazeserver)
