e50a96dffb3814b6baf5d6b5996efec769195ee2
Devashish Lal <laldevashish@gmail.com>
2023-12-04 15:43:44 -0800

    data loaded config loader

--- apps/CorgiAPI/DataLoader/Program.cs
+++ apps/CorgiAPI/DataLoader/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+Console.WriteLine($"App : {config["Name"]!}");
